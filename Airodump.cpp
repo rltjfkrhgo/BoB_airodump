f955e469fabe665c246c4b0bd2fcd59e4cd93c42
rltjfkrhgo <rltjfkrhgo@hanmail.net>
2021-01-26 16:19:12 +0900

    ssid

--- Airodump.cpp
+++ Airodump.cpp
@@ -6,11 +6,21 @@
 void airodump(const u_char* packet, u_int len)
 {
     RadiotapHdr* radiotapHdr = (RadiotapHdr*)packet;
-    uint16_t radiotapHdrLen = radiotapHdr->it_len;
-    Frame* frame = (Frame*)(packet+radiotapHdrLen);
+    int offset = radiotapHdr->it_len;
+    Frame* frame = (Frame*)(packet+offset);
 
     if(frame->type != Frame::BEACON_FRAME_TYPE)
         return;
 
     Mac bssid(frame->bssid);
+
+    offset += 24 + 12;
+    uint8_t* tagged = (uint8_t*)packet+offset;
+    
+    char ssid[256];
+    uint8_t ssidLen = tagged[1];
+    memcpy(ssid, &tagged[2], ssidLen);
+    ssid[ssidLen] = '\0';
+
+    std::cout << std::string(bssid) << "\t" << ssid << std::endl;
 }
